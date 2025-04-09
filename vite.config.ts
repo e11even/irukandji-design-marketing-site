03970aa5249b07ec7482f6e55797318a57428e38
Jayendra Sharan <jayendra.sharan@gmail.com>
2025-04-09 14:34:26 +0000

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "irukandji-design-marketing-site",
   build: {
     outDir: "./docs",
     emptyOutDir: true, // also necessary
